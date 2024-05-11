37e64f761c2cc6a10fe9627842598968d64b3469
jeffgrunewald <jeff.grunewald@gmail.com>
2024-05-11 03:42:58 -0400

    fix base64 decoding of key components

--- src/jwk_cache/base64_serde.rs
+++ src/jwk_cache/base64_serde.rs
@@ -1,4 +1,4 @@
-use base64::{engine::general_purpose::STANDARD, Engine};
+use base64::{engine::general_purpose::URL_SAFE, Engine};
 use serde::{Deserialize, Deserializer, Serializer};
 
 pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
@@ -7,7 +7,7 @@ where
 {
     use serde::de::Error;
     String::deserialize(deserializer).and_then(|string| {
-        STANDARD
+        URL_SAFE
             .decode(string)
             .map_err(|err| Error::custom(err.to_string()))
     })
@@ -19,5 +19,5 @@ where
     S: Serializer,
     B: AsRef<[u8]>,
 {
-    serializer.serialize_str(&STANDARD.encode(bytes.as_ref()))
+    serializer.serialize_str(&URL_SAFE.encode(bytes.as_ref()))
 }
